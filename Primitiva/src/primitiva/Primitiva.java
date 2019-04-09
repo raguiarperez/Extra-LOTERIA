f2cacde0aae5804cff7bd79379f63f2a4ae07852
raguiarperez <raguiarperez@nautilus8.danielcastelao.org>
2019-04-09 11:17:14 +0200

    Loteria Extra

---
+++ Primitiva/src/primitiva/Primitiva.java
@@ -0,0 +1,16 @@
+
+package primitiva;
+
+/**
+ *
+ * @author raguiarperez
+ */
+public class Primitiva {
+
+    public static void main(String[] args) {
+        Primitiv loteria= new Primitiv();
+        loteria.setVisible(true);
+        loteria.setDefaultCloseOperation(3);
+    }
+    
+}
